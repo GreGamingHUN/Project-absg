a6b2aba4d5d69fda7ccc60f4c813cbf5c289643b
GreG <prager.gergo@gmail.com>
2022-04-01 11:02:52 +0200

    JavaFX Scene

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -11,7 +13,8 @@ import java.util.Stack;
 
 public class Main extends Application {
 
-    Button button;
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,18 +22,39 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title");
+        window = primaryStage;
 
-        button = new Button();
-        button.setText("fasz");
-        button.setOnAction(e -> System.out.println("Fasz"));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        //Button 1
+        Label label1 = new Label("Szoszi devla");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> {
+            System.out.println("toScene 2");
+            window.setScene(scene2);
+        });
 
-        Scene scene = new Scene(layout, 300, 400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Layout 1
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+
+        //Button 2
+        Button button2 = new Button("Go to scene 1");
+        button1.setOnAction(e -> {
+            window.setScene(scene1);
+            System.out.println("toScene 1");
+        });
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        //Set Default Scene
+        window.setScene(scene1);
+        window.setTitle("ABSG");
+        window.show();
     }
 
 }
