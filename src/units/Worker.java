440f0b6ea88f23fd3c2638bf091e640b5e4eb1f0
GreG <prager.gergo@gmail.com>
2022-04-05 22:15:25 +0200

    Worker class update

--- src/units/Worker.java
+++ src/units/Worker.java
@@ -1,13 +1,36 @@
 package units;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Worker extends Unit{
-    private String pathToImg;
+    static String pathToImg = "img/villager.jpg";
+
+    public Worker() {
+        this.setImg(new ImageView(new Image(this.getPathToImg())));
+        this.getImg().setOnMouseEntered(e -> {
+            this.getImg().setOpacity(0.5);
+        });
+
+        this.getImg().setOnMouseExited(e -> {
+            this.getImg().setOpacity(1);
+        });
+
+        this.getImg().setOnMouseClicked(e -> {
+            System.out.println("Worker");
+        });
+    }
 
     public Worker(int price, int dmg, int hp, int speed, int priority, Hos parent) {
         super(price, dmg, hp, speed, priority, parent);
         this.setPrice(price);
-        this.setDmg(dmg);
+        this.setMinDmg(dmg);
         this.setHp(hp);
         this.setSpeed(speed);
         this.setPriority(priority);
+    }
+
+    public String getPathToImg() {
+        return pathToImg;
     }
 }
