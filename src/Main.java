554fc85a3813699aff9976264c8136c6b2a6863c
GreG <prager.gergo@gmail.com>
2022-04-05 22:16:42 +0200

    Add Unit selector Items
    They are not functional yet

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
 import javafx.scene.shape.Rectangle;
+import units.Archer;
+import units.Griff;
 import units.Tile;
+import units.Worker;
 
 import static javafx.scene.paint.Color.rgb;
 
@@ -22,6 +28,8 @@ public class Main extends Application {
     public int gameStage = -1;
     public static int difficulty = 1;
 
+    private static GridPane unitsGrid;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -34,6 +42,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Fasz");
         primaryStage.show();
+
     }
 
     static int gridSizeX = 12;
@@ -41,7 +50,8 @@ public class Main extends Application {
     static int rectSize = 30;
     static Tile[][] tileArray = new Tile[gridSizeX][gridSizeY];
 
-    public static void startGameMain(GridPane tilesGrid, int diff) {
+    public static void startGameMain(GridPane tilesGrid, GridPane uGrid, int diff) {
+        unitsGrid = uGrid;
         for (int i = 0; i < gridSizeX; i++) {
             for (int j = 0; j < gridSizeY; j++) {tileArray[i][j] = new Tile(new Rectangle(rectSize, rectSize), i, j);
                 tilesGrid.add(tileArray[i][j].getTileRect(), i, j);
@@ -49,12 +59,40 @@ public class Main extends Application {
             }
         }
         setDifficulty(diff);
+
+        //region selectorSetUp
+        Worker worker = new Worker();
+        ImageView workerImgView = worker.getImg();
+        workerImgView.setFitHeight(50);
+        workerImgView.setFitWidth(50);
+        unitsGrid.add(workerImgView, 0, 0);
+        unitsGrid.add(new Text("Földműves"), 0, 1);
+
+        Archer archer = new Archer();
+        ImageView archerImgView = archer.getImg();
+        archerImgView.setFitHeight(50);
+        archerImgView.setFitWidth(50);
+        unitsGrid.add(archerImgView, 1, 0);
+        unitsGrid.add(new Text("Íjász"), 1, 1);
+
+        Griff griff = new Griff();
+        ImageView griffImgView = griff.getImg();
+        griffImgView.setFitHeight(50);
+        griffImgView.setFitWidth(50);
+        unitsGrid.add(griffImgView, 2, 0);
+        unitsGrid.add(new Text("Griff"), 2, 1);
+        //endregion
     }
 
     public static void setDifficulty(int diff) {
         difficulty = diff;
         System.out.println(difficulty);
     }
+
+    public static void setUpUnitsGrid() {
+
+    }
+
 
 
 }
