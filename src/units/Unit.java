6c90b051141daeb906f0370726e004a7c180fb2c
GreG <prager.gergo@gmail.com>
2022-04-15 23:29:20 +0200

    Unit damage calculation

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -13,11 +13,14 @@ public class Unit {
     private int price;
     private int minDmg;
     private int maxDmg;
+    private int defaultHp;
     private int hp;
     private int speed;
     private int priority;
 
     private int unitAmount = 1;
+
+    private boolean visszatamadott = false;
 
     private String unitName;
     private String unitShort;
@@ -29,62 +32,75 @@ public class Unit {
 
     public void attack(Unit enemy) {
         Random r = new Random();
-        int dmgToEnemy = (int) ((r.nextInt(this.getMaxDmg()-this.getMinDmg()) + this.getMinDmg()) * (1 + (0.1 * this.getParentHos().getDmgUp())) -
-                (0.05 * enemy.getParentHos().getDefUp()));
+        int dmgToEnemy = (int)((1 - enemy.getParentHos().getDefUp() * 0.05) * ((this.getParentHos().getDmgUp() + 1) * r.nextInt(this.getMinDmg(), this.getMaxDmg() + 1)));
+        System.out.println(dmgToEnemy);
         enemy.setHp(enemy.getHp() - dmgToEnemy);
+        System.out.println(enemy.getHp());
+        enemy.getDamaged(this);
+    }
+
+    public void getDamaged (Unit target) {
+        Random r = new Random();
+        this.setUnitAmount(hp/defaultHp);
+        this.setMinDmg(this.getMinDmg()/this.getUnitAmount());
+        this.setMaxDmg(this.getMaxDmg()/this.getUnitAmount());
+        if (this.getHp() > 0 && !this.isVisszatamadott()) {
+            target.setHp((int)(target.getHp() - (r.nextInt(this.getMinDmg(), this.getMaxDmg() + 1)) * 0.05));
+        }
+    }
+
+    public void getDamaged() {
+        this.setUnitAmount(hp/defaultHp);
+        this.setMinDmg(this.getMinDmg()/this.getUnitAmount());
+        this.setMaxDmg(this.getMaxDmg()/this.getUnitAmount());
     }
 
     //region Getters
 
 
+    public boolean isVisszatamadott() {
+        return this.visszatamadott;
+    }
+
+    public int getDefaultHp() {
+        return this.defaultHp;
+    }
     public boolean isSelected() {
         return this.selected;
     }
-
     public String getUnitShort() {
         return this.unitShort;
     }
-
     public int getUnitAmount() {
         return this.unitAmount;
     }
-
     public String getUnitName() {
         return this.unitName;
     }
-
     public int getPrice() {
         return this.price;
     }
-
     public int getMinDmg() {
         return this.minDmg;
     }
-
     public int getHp() {
-        return this.hp * this.getUnitAmount();
+        return this.hp;
     }
-
     public int getSpeed() {
         return this.speed;
     }
-
     public int getPriority() {
         return this.priority;
     }
-
     public Hos getParentHos() {
         return this.parentHos;
     }
-
     public int getMaxDmg() {
         return this.maxDmg;
     }
-
     public int getPosX() {
         return this.posX;
     }
-
     public int getPosY() {
         return this.posY;
     }
@@ -94,54 +110,49 @@ public class Unit {
     //region Setters
 
 
+    public void setVisszatamadott(boolean visszatamadott) {
+        this.visszatamadott = visszatamadott;
+    }
+
+    public void setDefaultHp(int defaultHp) {
+        this.defaultHp = defaultHp;
+    }
     public void setSelected(boolean selected) {
         this.selected = selected;
     }
-
     public void setUnitShort(String unitShort) {
         this.unitShort = unitShort;
     }
-
     public void setUnitAmount(int unitAmount) {
         this.unitAmount = unitAmount;
     }
-
     public void setUnitName(String unitName) {
         this.unitName = unitName;
     }
-
     public void setPrice(int price) {
         this.price = price;
     }
-
     public void setMinDmg(int minDmg) {
         this.minDmg = minDmg;
     }
-
     public void setHp(int hp) {
         this.hp = hp;
     }
-
     public void setSpeed(int speed) {
         this.speed = speed;
     }
-
     public void setPriority(int priority) {
         this.priority = priority;
     }
-
     public void setParentHos(Hos parentHos) {
         this.parentHos = parentHos;
     }
-
     public void setMaxDmg(int maxDmg) {
         this.maxDmg = maxDmg;
     }
-
     public void setPosX(int posX) {
         this.posX = posX;
     }
-
     public void setPosY(int posY) {
         this.posY = posY;
     }
